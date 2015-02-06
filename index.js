b636cf859c1af9f691bdd462c867308b76b033b8
Michael Williams <dinosaur@riseup.net>
2015-02-06 19:04:55 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var Metalsmith = require('metalsmith');
+var plugins = require('load-metalsmith-plugins')();
+
+var m = Metalsmith(__dirname)
+.clean(false);
+
+if (process.argv[2] === '-w') {
+  m.use(plugins.watch());
+}
+
+m.use(plugins.collections({
+  resources: {
+    pattern: "resources/**/*.md",
+    sortBy: "order",
+  },
+}))
+.use(plugins.markdown())
+.use(plugins.templates('swig'))
+.build(function (err, files) {
+  if (err) { throw err; }
+  //console.log(files)
+})
+;
